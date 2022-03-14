af4c01cee10c3c93983f9b75126b6e0434df24cf
StephenDai <37321444+fideiailuai@users.noreply.github.com>
2022-03-14 23:02:24 +0800

    Add files via upload

---
+++ seach/seach/源.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int k = 0;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int left = 0;
+	int right = sz - 1;
+	printf("请输入一个0-10的值\n");
+	scanf_s("%d", &k);
+	while (left <= right)
+	{
+		int mid = (left + right) / 2;
+		if (arr[mid] < k)
+		{
+			left = mid + 1;
+		}
+		else if (arr[mid] > k)
+		{
+			right = mid - 1;
+		}
+		else
+		{
+			printf("找到了，下标是%d\n", mid);
+			break;
+		}
+	}
+	if (left > right)
+	{
+		printf("不存在需要查找的元素");
+	}
+	return 0;
+}
